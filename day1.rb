ce1bf9ad9eba9e41b6a81f2f9fe594f91b3f4f45
Irtasam <irtasamop@gmail.com>
2026-02-26 07:50:07 +0500

    Update day1.rb
    Task 2

--- day1.rb
+++ day1.rb
@@ -3,5 +3,11 @@ puts "Enter Your Name:"
 name = gets.chomp()
 puts "Hello " + name + "!"
 
+# Task 2
+puts "Enter your age:"
+age = gets.chomp().to_i
+next_year = age + 1
+puts "Next year you will be " + next_year.to_s + " years old."
+
 
 
