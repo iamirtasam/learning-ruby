424e05e95a886d1369b3ddbfb7c64f10b1a45dec
Irtasam <irtasamop@gmail.com>
2026-03-13 00:44:28 +0500

    Task 2

--- day3.rb
+++ day3.rb
@@ -8,3 +8,13 @@ age = gets.chomp().to_i
 
 puts "Hello" + " " + name + "!"
 puts "Next year you will be " + (age + 1).to_s + " years old"
+
+# Task 2
+
+puts "Enter a number: "
+num = gets.chomp().to_i
+
+if num % 2 == 0
+  puts "The number is even"
+else puts "The number is odd"
+end
