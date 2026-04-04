ae5f99139b313b44350ca558e290fc58e7068d9f
Irtasam <irtasamop@gmail.com>
2026-04-04 09:22:27 +0500

    Update hhhhh.rb

--- hhhhh.rb
+++ hhhhh.rb
@@ -14,3 +14,20 @@ puts 100 % 10
 puts 15 % 2
 # 5. Result of 24 % 6
 puts 24 % 6
+
+
+# Initial balance
+balance = 200
+
+# TODO: Write your code below
+# Use compound assignment operators (+=, -=, *=, /=, %=) to:
+# 1. Add 50 to balance, then print it
+puts balance += 50
+# 2. Subtract 30 from balance, then print it
+puts balance -= 30
+# 3. Multiply balance by 2, then print it
+puts balance *= 2
+# 4. Divide balance by 4, then print it
+puts balance /= 4
+# 5. Get the remainder when balance is divided by 7, then print it
+puts balance %= 7
