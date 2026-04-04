9fbbcb00a7f3c9430c817444a3ae8b5683587edf
Irtasam <irtasamop@gmail.com>
2026-04-04 09:04:34 +0500

    Create hhhhh.rb

---
+++ hhhhh.rb
@@ -0,0 +1,16 @@
+# Variables provided
+total_items = 23
+items_per_box = 5
+
+# TODO: Write your code below
+# Use the modulo operator (%) to calculate and print:
+# 1. Remainder when dividing total_items by items_per_box
+puts total_items % items_per_box
+# 2. Result of 17 % 4
+puts 17 % 4
+# 3. Result of 100 % 10
+puts 100 % 10
+# 4. Result of 15 % 2
+puts 15 % 2
+# 5. Result of 24 % 6
+puts 24 % 6
